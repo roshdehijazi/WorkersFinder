c7322b739db78b1173d2cb7445d55ce9f74a7eda
roshdehijazi <roshdema@gmail.com>
2025-04-13 00:27:43 +0300

    Initial commit

---
+++ src/test/java/FindWorkers/WorkersFinder/WorkersFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package FindWorkers.WorkersFinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkersFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
