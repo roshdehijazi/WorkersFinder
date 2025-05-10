d3db1b48e3c3146602c58dedf3041a79e21bda7e
roshdehijazi <roshdema@gmail.com>
2025-05-10 23:47:43 +0300

    back to old

--- src/main/java/FindWorkers/WorkersFinder/IssueService.java
+++ src/main/java/FindWorkers/WorkersFinder/IssueService.java
@@ -11,6 +11,7 @@ public class IssueService {
     private IssueRepository issueRepository;
 
     public Issue createIssue(Issue issue) {
+
         return issueRepository.save(issue);
     }
 
@@ -31,13 +32,12 @@ public class IssueService {
         return issueRepository.findAllByOrderByStartDateAsc();
     }
 
-
-
     public void deleteIssue(String issueId){
         if(!issueRepository.existsById(issueId))
             throw new RuntimeException("User not found with id: " + issueId);
         issueRepository.deleteById(issueId);
     }
+
     public Issue addViewer (String issueId){
         Issue issue = issueRepository.findById(issueId)
                 .orElseThrow(() -> new RuntimeException("Issue not found"));
@@ -45,43 +45,53 @@ public class IssueService {
         count++;
         issue.setCountViewrs(count);
         return issueRepository.save(issue);
-
     }
+
     public List<Issue> getAllByCategory(Category category){
         return issueRepository.findByCategoryOrderByStartDateDesc(category);
     }
+
     public List<Issue> getAllByCategoryOlder(Category category){
         return issueRepository.findByCategoryOrderByStartDateAsc(category);
     }
+
     public Issue addWorker (String issueId,String workerId){
         Issue issue = issueRepository.findById(issueId)
                 .orElseThrow(() -> new RuntimeException("Issue not found"));
         issue.setWorkerId(workerId);
         return issueRepository.save(issue);
     }
+
     public Issue updateImage (String issueId ,String image){
         Issue issue = issueRepository.findById(issueId)
                 .orElseThrow(() -> new RuntimeException("Issue not found"));
         issue.setPicture(image);
         return issueRepository.save(issue);
     }
+
+
     public Issue updateTitle (String issueId ,String title){
         Issue issue = issueRepository.findById(issueId)
                 .orElseThrow(() -> new RuntimeException("Issue not found"));
         issue.setTitle(title);
         return issueRepository.save(issue);
     }
+
+
     public Issue updateCategory (String issueId ,Category category){
         Issue issue = issueRepository.findById(issueId)
                 .orElseThrow(() -> new RuntimeException("Issue not found"));
         issue.setCategory(category);
         return issueRepository.save(issue);
     }
+
+
     public Issue updateDescription (String issueId ,String description){
         Issue issue = issueRepository.findById(issueId)
                 .orElseThrow(() -> new RuntimeException("Issue not found"));
         issue.setDescription(description);
         return issueRepository.save(issue);
     }
+
 
 }
--- src/main/java/FindWorkers/WorkersFinder/Users/User.java
+++ src/main/java/FindWorkers/WorkersFinder/Users/User.java
@@ -56,7 +56,6 @@ public class User {
     public void setId(String id) {
        this. id = id;
     }
-
     public String getUsername() { return username; }
     public void setUsername(String username) { this.username = username; }
 
